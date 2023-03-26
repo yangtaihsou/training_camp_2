63d2191cb746ed2ebfe5c77366f10ae8cc9c6cb0
Jim <yangtaishou@gmail.com>
2023-03-26 22:20:50 +0800

    增加单元测试

--- README.md
+++
@@ -1,3 +0,0 @@
-# training_camp_2
-solidity学习
-登链区块链技术集训营第二期作业
--- learnHomework/w2-2/test/TeacherController.js
+++ learnHomework/w2-2/test/TeacherController.js
@@ -14,13 +14,59 @@ describe("TeacherController", function () {
         const teacherController = await TeacherController.deploy();
         await teacherController.deployed();
         console.log("teacherController contract address:", teacherController.address);
-        return { teacherController, owner, otherAccount };
+
+        const ScoreStore = await ethers.getContractFactory("ScoreStore");
+        const scoreStore = await ScoreStore.deploy();
+        await scoreStore.deployed();
+
+        return { teacherController, owner, otherAccount, scoreStore };
     }
 
 
 
-    it("deploy", async function () {
-        
+    it("verify deploy ScoreStore", async function () {
+
         const { teacherController, owner, otherAccount } = await loadFixture(init);
     });
+
+    it("add a teacher", async function () {
+
+        const { teacherController, owner, otherAccount } = await loadFixture(init);
+
+        await teacherController.connect(owner).addTeacher(otherAccount.address);
+
+        expect(await teacherController.connect(owner).getTeacher(otherAccount.address))
+            .equal(otherAccount.address);
+    });
+
+    it("setStudentScore", async function () {
+        const { teacherController, owner, otherAccount, scoreStore } = await loadFixture(init);
+
+        await teacherController.connect(owner).addTeacher(otherAccount.address);
+
+        const studentAddress = "0x032df3014aA46cAa53D324fB006b39eDd163e27d";
+        await teacherController.connect(otherAccount).setStudentScore(
+            studentAddress, "math",
+            90);
+
+        //通过老师控制器查询学生分数。老师控制器部署分数合约
+        expect(await teacherController.connect(otherAccount).getStudentScore(studentAddress, "math"))
+            .equal(90);
+
+
+        const scoreStoreAddress = await teacherController.connect(otherAccount).getScoreStoreAddress();
+        console.log("ScoreStore contract address:%s", scoreStoreAddress);
+        //通过分数合约scoreStore，直接连接合约地址，数据为空。待咨询
+        expect(await scoreStore.connect(scoreStoreAddress).getScoreByStudent(studentAddress, "math"))
+            .equal(0);
+
+       /* const provider = ethers.provider;
+        const abi = ["function getScoreByStudent(address student,string) external view returns (uint8)"];
+        const contract = ethers.Contract(scoreStoreAddress, abi, provider);
+        expect(await contract.getScoreByStudent(studentAddress, "math")).equal(0);*/
+
+
+    });
+
+
 })
